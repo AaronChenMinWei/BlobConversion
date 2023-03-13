bb26a06175a2a020b3f77bf8b6c94ee07ca12a60
Aaron Chen <aaron.mw.chen@avanade.com>
2023-03-13 23:32:30 +0800

    Add project files.

---
+++ BlobConversion/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using BlobConversion.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BlobConversion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            byte[] blobArray = System.IO.File.ReadAllBytes("BlobData/Blob.pdf");
+            string base64String = $"data:application/pdf;base64,{Convert.ToBase64String(blobArray)}";
+            var model = new Blob(base64String, blobArray.Length);
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 300, VaryByQueryKeys = new[] { "*" })]
+        public FileContentResult PdfBlob()
+        {
+            var pdfBlob = System.IO.File.ReadAllBytes("BlobData/Blob.pdf");
+            return new FileContentResult(pdfBlob, "application/pdf");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private string ConvertToBase64(Stream stream)
+        {
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
---
+++ BlobConversion/Models/Blob.cs
@@ -0,0 +1,14 @@
+﻿namespace BlobConversion.Models
+{
+    public class Blob
+    {
+        public Blob(string _base64String, int _sizeInByte)
+        {
+            Base64String = _base64String;
+            SizeInByte = _sizeInByte;
+        }
+
+        public string Base64String { get; set; }
+        public int SizeInByte { get; set; }
+    }
+}
